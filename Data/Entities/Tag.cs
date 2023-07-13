f227415fa6db0f3332327fb5bfa6b3925f40e125
Maina <simonmaina721@gmail.com>
2023-07-13 10:58:31 +0300

    Added new entity relationships to the Book entity class

---
+++ Data/Entities/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstEfCoreApp.Data.Entities
+{
+    public class Tag
+    {
+        [Key]
+        [Required]
+        [MaxLength(40)]
+        public string TagId { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
